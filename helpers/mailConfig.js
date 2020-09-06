ce0587731d9abe86608d2b42f67a7973516597ac
prabutime <prabu.kalaivanan@time.com.my>
2020-09-06 14:23:05 +0800

    added service gmail

--- helpers/mailConfig.js
+++ helpers/mailConfig.js
@@ -3,6 +3,7 @@ var nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
     auth: {
